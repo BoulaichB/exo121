65c22fda7179ac1a9631fdf172f8d67e9f401c3c
Bachir Boulaich <bachir.boulaich@outlook.com>
2023-02-08 14:12:55 +0100

    Search

--- src/components/search/search.js
+++ src/components/search/search.js
@@ -2,10 +2,10 @@ import React from 'react'
 
 const Search = () => {
   return (
-    <section>
-        <div>
-            <h3>Search Your Favorite Cocktail</h3>
-            <input type="text" />
+    <section className='my-20'>
+        <div className='mx-auto w-1/2 bg-white px-12 py-8 shadow-md shadow-black rounded'>
+            <h3 className='text-green-900 font-bold tracking-widest mb-4'>Search Your Favorite Cocktail</h3>
+            <input type="text" className='bg-gray-100 w-full'/>
         </div>
     </section>
   )
